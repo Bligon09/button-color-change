9a65976eb68fd8461ae99ea5329aff131c16bceb
Jon Ohliger <jon1213@gmail.com>
2023-08-27 16:21:15 -0700

    made background-color red

--- ButtonCC/src/components/Button.jsx
+++ ButtonCC/src/components/Button.jsx
@@ -6,6 +6,8 @@ const Button = ( {position} ) => {
   return (<button
     position={position}
     className="btn btn-default"
+    style={{backgroundColor: "red", backgroundImage: "none"}} 
+    // removing background image since that is how we got the gradients working in App.css, and currently background-image takes precedence over background-color
     onClick={() => setCount((count) => count + 1)}
   >
     Position is {position[0] + ','+position[1]} <br />
