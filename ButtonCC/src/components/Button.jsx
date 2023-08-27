6c058a66c7c06c7a811bf44b0428808600866f7c
Jon Ohliger <jon1213@gmail.com>
2023-08-27 16:37:14 -0700

    fixed the bug using modulo

--- ButtonCC/src/components/Button.jsx
+++ ButtonCC/src/components/Button.jsx
@@ -11,7 +11,7 @@ const Button = ( {position} ) => {
     className="btn btn-default"
     style={{backgroundColor: bgc, backgroundImage: "none"}} 
     // removing background image since that is how we got the gradients working in App.css, and currently background-image takes precedence over background-color
-    onClick={() => setBackgroundColor((bgc) => colors[colors.indexOf(bgc) + 1])}
+    onClick={() => setBackgroundColor((bgc) => colors[(colors.indexOf(bgc) + 1) % colors.length ])}
   >
     Position is {position[0] + ',' + position[1]} <br />
   </button>
