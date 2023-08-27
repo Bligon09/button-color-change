beb1e10f02f99e3324aea898112568c5cc6481f6
Jon Ohliger <jon1213@gmail.com>
2023-08-27 16:35:39 -0700

    Almost there; current rotation has an array index bug in the function, but otherwise it is working

--- ButtonCC/src/components/Button.jsx
+++ ButtonCC/src/components/Button.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 
 const Button = ( {position} ) => {
   const [count, setCount] = useState(0);
-  const [bgc, setBgc] = useState('red');
+  const [bgc, setBackgroundColor] = useState('red');
 
   const colors = ['red', 'green', 'blue'];
 
@@ -11,7 +11,7 @@ const Button = ( {position} ) => {
     className="btn btn-default"
     style={{backgroundColor: bgc, backgroundImage: "none"}} 
     // removing background image since that is how we got the gradients working in App.css, and currently background-image takes precedence over background-color
-    onClick={() => setBgc((bgc) => 'green')}
+    onClick={() => setBackgroundColor((bgc) => colors[colors.indexOf(bgc) + 1])}
   >
     Position is {position[0] + ',' + position[1]} <br />
   </button>
