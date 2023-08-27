6d2f8594693d727d79d23247b6f69163571cd4c6
Jon Ohliger <jon1213@gmail.com>
2023-08-27 16:28:59 -0700

    onclick changes button background color to green

--- ButtonCC/src/components/Button.jsx
+++ ButtonCC/src/components/Button.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 
 const Button = ( {position} ) => {
   const [count, setCount] = useState(0);
+  const [bgc, setBgc] = useState('red');
 
   return (<button
     position={position}
     className="btn btn-default"
-    style={{backgroundColor: "red", backgroundImage: "none"}} 
+    style={{backgroundColor: bgc, backgroundImage: "none"}} 
     // removing background image since that is how we got the gradients working in App.css, and currently background-image takes precedence over background-color
-    onClick={() => setCount((count) => count + 1)}
+    onClick={() => setBgc((bgc) => 'green')}
   >
-    Position is {position[0] + ','+position[1]} <br />
+    Position is {position[0] + ',' + position[1]} <br />
   </button>
   );
 }
